10cc38c92aa7e33e7dc7139eaff56904dcfc98b0
JustinCheung6 <justincheung6@gmail.com>
2022-07-27 20:29:01 -0700

    - Added player movement relative to camera - Attempted to add player rotation relative to camera To Do: - Figure out player rotation relative to camera

--- HacknSad/Assets/Scripts/PlayerController.cs
+++ HacknSad/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,8 @@ namespace Player
     {
         #region Components
         private CharacterController controller;
-        
+        private Transform cameraTrans;
+
         //Player Input
         private PlayerInput pInput;
         private InputAction moveAction;
@@ -30,6 +31,7 @@ namespace Player
             controller = GetComponent<CharacterController>();
             pInput = GetComponent<PlayerInput>();
             animator = GetComponentInChildren<Animator>();
+            cameraTrans = Camera.main.transform;
 
             //Get input actions from PlayerInput
             moveAction = pInput.actions["Movement"];
@@ -75,10 +77,26 @@ namespace Player
             {
                 animator.SetBool("Moving", moveInput != Vector3.zero);
 
-                if(moveInput != Vector3.zero)
+                //Get the forward and right vectors of the camera
+                Vector3 forward = new Vector3(cameraTrans.forward.x, 0f, cameraTrans.forward.z).normalized;
+                Vector3 right = new Vector3(cameraTrans.right.x, 0f, cameraTrans.right.z).normalized;
+
+                
+
+                //Get move vector relative to camera forward
+                Vector3 movement = (forward * moveInput.z) + (right * moveInput.x);
+
+                if (moveInput != Vector3.zero)
                 {
-                    controller.Move(Time.fixedDeltaTime * moveSpeed * moveInput);
-                    transform.rotation = Quaternion.Euler(0f, UpdateRotation(moveInput.x, moveInput.z), 0f);
+                    controller.Move(Time.fixedDeltaTime * moveSpeed * movement);
+
+                    //Debug.Log("Cam: " + forward + " Move: " + movement);
+
+                    float rotation = cameraTrans.rotation.eulerAngles.y - RotationOffset(forward, movement);
+
+                    //Debug.Log(rotation);
+
+                    transform.rotation = Quaternion.Euler(0f, rotation, 0f);
                 }
             }
             catch (NullReferenceException e)
@@ -91,26 +109,23 @@ namespace Player
             }
         }
 
-        private float UpdateRotation(float x, float y)
+        //Get angle distance between 2 points (camera forward, player direction)
+        private float RotationOffset(Vector3 f, Vector3 p)
         {
-            if (x == 0f)
-            {
-                return (y >= 0f) ? 0f : 180f;
-            }
-            else if (y == 0f)
-                return (x > 0f) ? 90f : 270f;
+            //case where player is running forward
+            if (f == p)
+                return 0f;
 
+            Vector3 relative = cameraTrans.InverseTransformDirection(p);
 
-            float angle = Mathf.Atan(Mathf.Abs(y / x)) * 180 / Mathf.PI;
+            float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
 
-            if (x >= 0 && y >= 0)
-                return angle;
-            else if (x >= 0 && y < 0)
-                return angle + 90;
-            else if (x < 0 && y < 0)
-                return angle + 180;
-            else
-                return angle + 270;
+            //float angle = Mathf.Atan2(p.x - f.x, p.z - f.z) * Mathf.Rad2Deg;
+            //angle = (angle * 2) + 180;
+
+            Debug.Log(angle);
+
+            return angle;
         }
     }
 }
